b7fcc46abf9fb426ee978c4005d0e1bd86ef999f
ApoG4mer <andrewpresta22@gmail.com>
2024-02-02 13:16:02 -0500

    Updated Files

--- etch.js
+++ etch.js
@@ -1,30 +1,57 @@
 const grid = document.querySelector('.grid');
+let blockSize = 20;
 
-for(let i = 0; i < 256; i++)
+
+const slider = document.getElementById('slider-picker');
+const sliderText = document.getElementById('slider-text');
+let block = document.querySelectorAll('.block');
+
+let sliderValue = slider.value;
+
+//set up the base grid
+getGrid(16);
+
+slider.addEventListener('mousemove', () =>
 {
-    const div = document.createElement('div');
-    div.className = "block-item";
-    //the amount of pixels is 320 / grid size
-    //we want to change the amount of blocks and block size (not actually changing the grid size)
-    div.style.height = "20px";
-    div.style.width = "20px";
-    grid.append(div);
-}
-const blocks = document.querySelectorAll('.block-item');
-const colorPicker = document.getElementById('colorPicker');
-blocks.forEach(function (block) {
-    block.addEventListener('mouseover', () => {
-        let selColor = colorPicker.value;
-        block.style.backgroundColor = selColor;
-    })
+   clearGrid();
+   sliderValue = slider.value;
+   sliderText.textContent = sliderValue + " x " + sliderValue;
+   getGrid(sliderValue);
 });
 
-const clearButton = document.getElementById('clear-button');
-clearButton.addEventListener('click', () => {
-    blocks.forEach(function (block) {
-        block.style.backgroundColor = "white";
-    });
-})
-colorPicker.addEventListener('click', () => {
 
-})
+let clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', () => 
+{
+   let block = document.querySelectorAll('.block');
+   block.forEach(function (e) {e.style.backgroundColor = 'white'})
+});
+
+function getGrid(val)
+{
+   for(let i = 0; i < val * val; i++)
+   {
+       const grid = document.querySelector('.grid');
+       const div = document.createElement('div');
+       let math = 320/sliderValue;
+       div.style.height = math + 'px';
+       div.style.width = math + 'px';
+       div.className = 'block';
+       grid.appendChild(div);
+   }
+   finalizeGrid();
+}
+function finalizeGrid()
+{
+   let block = document.querySelectorAll('.block');
+   let colorButton = document.getElementById('colorPicker');
+   block.forEach(function (element) {element.addEventListener('mouseover', () => 
+   {
+      let cB = colorButton.value;
+      element.style.backgroundColor = cB;
+   })});
+}
+function clearGrid()
+{
+   grid.innerHTML = '';
+}
